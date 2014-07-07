2072ff9e5cdaa0e99fec0acf3813e265c80345cb
labra <jelabra@gmail.com>
2014-07-07 18:43:50 +0200

    Includes ids of validators

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import es.weso.shex.Schema
 
 object Application extends Controller {
 
@@ -10,7 +11,7 @@ object Application extends Controller {
   }
 
   def about = Action {
-    Ok(views.html.about())
+    Ok(views.html.about(Schema.id))
   }
   
 }
