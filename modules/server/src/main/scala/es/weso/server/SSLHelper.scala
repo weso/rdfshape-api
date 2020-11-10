5dfd1aa7a4209de85ee2d4d53aec9757994c6fe6
ulitol97 <eduulitol@gmail.com>
2020-11-10 12:52:18 +0100

    Serving via HTTPS when certificates are found on the server

--- modules/server/src/main/scala/es/weso/server/SSLHelper.scala
+++ modules/server/src/main/scala/es/weso/server/SSLHelper.scala
@@ -1,11 +1,12 @@
 package es.weso.server
 
+import java.io.{FileInputStream, IOException}
+import java.nio.file.Paths
+import java.security.{GeneralSecurityException, KeyStore, SecureRandom, Security}
+
 import cats.effect.{IO, Sync}
 import cats.implicits._
-import java.nio.file.Paths
-import java.security.{KeyStore, Security}
-
-import javax.net.ssl.{KeyManagerFactory, SSLContext}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 import org.http4s.HttpApp
 import org.http4s.Uri.{Authority, RegName, Scheme}
 import org.http4s.dsl.Http4sDsl
@@ -13,21 +14,51 @@ import org.http4s.headers.{Host, Location}
 import org.http4s.server.SSLKeyStoreSupport.StoreInfo
 
 object SSLHelper {
-  val keystorePassword: String = "password"
-  val keyManagerPassword: String = "wesopassword"
+  val keyStorePassword: String   = sys.env.getOrElse("KEYSTORE_PASSWORD", "")
+  val keyManagerPassword: String = sys.env.getOrElse("KEYMANAGER_PASSWORD", "")
+  val keyStorePath: String       = Paths.get(sys.env.getOrElse("KEYSTORE_PATH", "")).toAbsolutePath.toString
+  val storeInfo: StoreInfo       = StoreInfo(keyStorePath, keyStorePassword)
 
-  // val keystorePath: String = Paths.get("../server.jks").toAbsolutePath.toString
+  @throws(classOf[IOException])
+  @throws(classOf[GeneralSecurityException])
+  def getContext: SSLContext = {
+    if (keyStorePassword == "" || keyManagerPassword == "" || keyStorePath == "") {
+      SSLContext.getDefault
+    } else {
+      try{
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType)
 
-  // val storeInfo: StoreInfo = StoreInfo(keystorePath, keystorePassword)
-  /*def handler(e: Throwable): IO[SSLContext] = for {
-    _ <- IO( println(s"Exception: $e"))
-  } yield SSLContext.getDefault */
+        val in = new FileInputStream(keyStorePath)
+        keyStore.load(in, keyStorePassword.toCharArray)
+
+        val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+        keyManagerFactory.init(keyStore, keyStorePassword.toCharArray)
+
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+        trustManagerFactory.init(keyStore)
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom())
+        sslContext
+      }
+      catch {
+        case io: IOException => SSLContext.getDefault
+        case sec: IOException => SSLContext.getDefault
+      }
+    }
+  }
+
+  def handler(e: Throwable): IO[SSLContext] =
+    for {
+      _ <- IO(println(s"Exception: $e"))
+    } yield SSLContext.getDefault
 
   def loadContextFromClasspath[F[_]](keystorePassword: String, keyManagerPass: String)(
-    implicit F: Sync[F]): F[SSLContext] =
+      implicit F: Sync[F]
+  ): F[SSLContext] =
     F.delay {
       val ksStream = this.getClass.getResourceAsStream("/server.jks")
-      val ks = KeyStore.getInstance("JKS")
+      val ks       = KeyStore.getInstance("JKS")
       ks.load(ksStream, keystorePassword.toCharArray)
 
       println(s"KeyStore loaded: $ks")
@@ -36,12 +67,12 @@ object SSLHelper {
 
       val kmf = KeyManagerFactory.getInstance(
         Option(Security.getProperty("ssl.KeyManagerFactory.algorithm"))
-          .getOrElse(KeyManagerFactory.getDefaultAlgorithm))
+          .getOrElse(KeyManagerFactory.getDefaultAlgorithm)
+      )
 
       println(s"kmf before init: $kmf")
 
       kmf.init(ks, keyManagerPass.toCharArray)
-
 
       println(s"kmf after init: $kmf")
 
@@ -69,7 +100,10 @@ object SSLHelper {
               Authority(
                 userInfo = request.uri.authority.flatMap(_.userInfo),
                 host = RegName(host),
-                port = securePort.some)))
+                port = securePort.some
+              )
+            )
+          )
           MovedPermanently(Location(baseUri.withPath(request.uri.path)))
         case _ =>
           BadRequest()
