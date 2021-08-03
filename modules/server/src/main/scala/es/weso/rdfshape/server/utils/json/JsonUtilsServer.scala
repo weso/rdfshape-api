b34aa26b08f9d23d4d4e230e605d20ad07c2d1e4
ulitol97 <eduulitol@protonmail.com>
2021-08-03 13:23:43 +0200

    Documented JSON utils.

--- modules/server/src/main/scala/es/weso/rdfshape/server/utils/json/JsonUtilsServer.scala
+++ modules/server/src/main/scala/es/weso/rdfshape/server/utils/json/JsonUtilsServer.scala
@@ -2,7 +2,19 @@ package es.weso.rdfshape.server.utils.json
 
 import io.circe.Json
 
+/** Helper utilities to extract JSON from the complex data managed by the API.
+  */
 object JsonUtilsServer {
+
+  /** Converts some object to JSON, given a converter function.
+    *
+    * @param data Data to be converted to JSON
+    * @param name Name given to the data
+    * @param cnv  Converter function from A to Json
+    * @tparam A Type of the data to be converted to JSON
+    * @return A list with containing a single tuple: the name given to the data and the JSON representation of "A" itself.
+    *         The list will be empty if no data is provided for conversion.
+    */
   def maybeField[A](
       data: Option[A],
       name: String,
@@ -13,6 +25,15 @@ object JsonUtilsServer {
       case Some(v) => List((name, cnv(v)))
     }
 
+  /** Converts some object to JSON, given a converter function.
+    *
+    * @param data Data to be converted to JSON
+    * @param name Name given to the data
+    * @param cnv  Converter function from A to Json
+    * @tparam A Type of the data to be converted to JSON
+    * @return A list with containing a single tuple: the name given to the data and the JSON representation of "A" itself.
+    *         In case no data is provided, the list will contain: the name given to the data and the message given instead of the data.
+    */
   def eitherField[A](
       data: Either[String, A],
       name: String,
