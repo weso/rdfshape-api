570b80c769b77bbcef67baec395b1c201281c2f0
ulitol97 <eduulitol@protonmail.com>
2021-08-13 17:40:50 +0200

    Refactored values.

--- modules/server/src/main/scala/es/weso/rdfshape/server/api/values/WikidataEntityValue.scala
+++ modules/server/src/main/scala/es/weso/rdfshape/server/api/values/WikidataEntityValue.scala
@@ -1,3 +1,7 @@
 package es.weso.rdfshape.server.api.values
 
+/** Data class representing a Wikidata entity
+  *
+  * @param entity Entity of which the data is contained
+  */
 case class WikidataEntityValue(entity: Option[String])
