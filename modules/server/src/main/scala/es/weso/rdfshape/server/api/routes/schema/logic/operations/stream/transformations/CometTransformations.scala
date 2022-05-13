887cf181f37824bc7f0ae8e97f80c058b0806548
ulitol97 <eduulitol@protonmail.com>
2022-05-13 10:16:10 +0200

    Reduced timeout before closing a streaming validation.

--- modules/server/src/main/scala/es/weso/rdfshape/server/api/routes/schema/logic/operations/stream/transformations/CometTransformations.scala
+++ modules/server/src/main/scala/es/weso/rdfshape/server/api/routes/schema/logic/operations/stream/transformations/CometTransformations.scala
@@ -49,7 +49,7 @@ private[schema] object CometTransformations extends LazyLogging {
   /** Time that the server will wait without receiving any items for the input
     * stream before raising a [[StreamTimeoutException]]
     */
-  private lazy val timeout = 1 minute
+  private lazy val timeout = 40 seconds
 
   /** Stream transformation pipe:
     * 1. Use the configuration to a stream validation operation as input
